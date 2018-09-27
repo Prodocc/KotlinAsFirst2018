f16d7248a69aafb373017fdfa1cced81ffdfc722
Vlad <mobila1997@mail.ru>
2018-09-27 23:03:15 +0300

    Lesson2.task2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -42,24 +42,12 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = (x1==x2||y1==y
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
 fun daysInMonth(month: Int, year: Int): Int {
-    if ((month==2)&&((year%4==0)&&(year%100!=0)||(month==2)&&(year%400==0)&&(year%100==0))){
-            return 29
-    }else
-       when (month) {
-            1 -> return 31
-            2 -> return 28
-            3 -> return 31
-            4 -> return 30
-            5 -> return 31
-            6 -> return 30
-            7 -> return 31
-            8 -> return 31
-            9 -> return 30
-            10 -> return 31
-            11 -> return 30
-            12 -> return 31
-        }
-    return 0
+    when (month) {
+        1, 3, 5, 7, 8, 10, 12 -> return 31
+        4, 6, 9, 11 -> return 30
+        2 -> if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) return 29
+    }
+     return 28
 }
 
 /**
